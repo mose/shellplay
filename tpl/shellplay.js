30a83e441e6438ab0c5374888fc0df598f527486
mose <mose@mose.com>
2014-08-19 02:36:55 +0800

    fix html navigation

--- tpl/shellplay.js
+++ tpl/shellplay.js
@@ -1,6 +1,41 @@
 (function(){
 
-  $(".screen").hide();
-  $("#s0").show();
+  var current = 0;
+
+  function draw() {
+    $(".screen").hide();
+    $("#s"+current).show();
+  }
+
+  function next() {
+    current++;
+    draw();
+  }
+
+  function previous() {
+    console.log(current);
+    if (current > 0) {
+      current--;
+    }
+    draw();
+  }
+
+  draw();
+
+  $(document).keydown(function(e) {
+    switch(e.keyCode) {
+      case 39: // ->
+      case 78: // n
+      case 13: // enter
+        next();
+        break;
+      case 37: // <-
+      case 80: // p
+        previous();
+        break;
+      default:
+        console.log(e.keyCode);
+    }
+  });
 
 }());
